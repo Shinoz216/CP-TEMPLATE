0b0b913b62fa3743814a4d720c37961bfeebcf10
Shinoz <101450846+Shinoz216@users.noreply.github.com>
2025-09-13 20:47:46 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define ll long long
+#define bit(n, i) ((n >> i) & 1)
+#define all(a) a.begin(), a.end()
+#define rep(i, x, n) for (int i = x; i <= n; ++i)
+#define red(i, x, n) for (int i = x; i >= n; --i)
+#define inp(a) freopen(a".inp", "r", stdin), freopen(a".out", "w", stdout)
+
+template<class A, class B> inline bool maxi(A &x, B y) {return (x < y ? x = y, 1 : 0);};
+template<class A, class B> inline bool mini(A &x, B y) {return (x > y ? x = y, 1 : 0);};
+
+const int N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = LLONG_MAX;
+
+// main program
+
+
+signed main(){
+    cin.tie(0) -> sync_with_stdio(0);
+
+    
+
+    return 0;
+}
